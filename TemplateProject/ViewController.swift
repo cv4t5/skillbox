2e6b022c5ca5b5e8d264f5e7646937bee2b60da9
Roman Zakharchenko <r.zaharchenko23@gmail.com>
2024-02-18 05:27:51 +0200

    feat: add some functions
    add func which convert int arr to string arr; add func which string arr concatinate to single string; add func which call to func given from args with delay 2 sec; add func which return ref on two functions which was given from args;

--- TemplateProject/ViewController.swift
+++ TemplateProject/ViewController.swift
@@ -1,12 +1,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var arrayOfNumbers: [Int] = [5, 2, 4, 6, 89, 1, 3, 14, 77, 66]
+    typealias MyAlias = () -> Void
+
+    var arrayOfNumbers = [5, 2, 4, 6, 89, 1, 3, 14, 77, 66]
+    var arrayOfNames = ["Roma", "Sanya", "Vladik", "Vasya", "Pertya"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         sortAndPrintArray(array: arrayOfNumbers)
+        print(convertArrayNumbersToArrayString(array: arrayOfNumbers))
+        print(convertStringArrayToSingleString(array: arrayOfNames))
+        executeSomeFunc(someFunc: { print("hello") })
+
+        var x = getReferenceForExecuteTwoFunc(func1: { print("123") }, func2: { print("321") })
+        x()
     }
 
     func sortAndPrintArray(array: [Any]) {
@@ -14,5 +23,40 @@ class ViewController: UIViewController {
             let sortedArray = array.sorted(by: >)
             print(sortedArray)
         }
+    }
+
+    func convertArrayNumbersToArrayString(array: [Int]) -> [String] {
+        let strNumbers = array.map { "\($0)" }
+        return strNumbers
+    }
+
+    func convertStringArrayToSingleString(array: [String]) -> String {
+        let singleString = array.reduce("") { $0 + "\($1) " }
+        return singleString
+    }
+
+    func executeSomeFunc(someFunc: @escaping () -> Void) {
+        print("Main func execute at \(getStringDateTimeNow())")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            print("Some func execute at \(self.getStringDateTimeNow())")
+            someFunc()
+        }
+    }
+
+    func getReferenceForExecuteTwoFunc(func1: @escaping MyAlias, func2: @escaping MyAlias) -> MyAlias {
+        { func1()
+            func2()
+        }
+    }
+
+    func getStringDateTimeNow() -> String {
+        // get the current date and time
+        let currentDateTime = Date()
+
+        // initialize the date formatter and set the style
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .none
+        return formatter.string(from: currentDateTime) // "10:52:30"
     }
 }
