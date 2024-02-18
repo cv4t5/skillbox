c1a33c3620cfa196b684add710b7dedccfc810d5
Roman Zakharchenko <r.zaharchenko23@gmail.com>
2024-02-18 03:09:49 +0200

    feat: add sort method
    Add method which can sort any type of array by ASC

--- TemplateProject/ViewController.swift
+++ TemplateProject/ViewController.swift
@@ -1,15 +1,18 @@
-//
-//  ViewController.swift
-//  TemplateProject
-//
-//  Created by Роман Захарченко on 07.02.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    var arrayOfNumbers: [Int] = [5, 2, 4, 6, 89, 1, 3, 14, 77, 66]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        sortAndPrintArray(array: arrayOfNumbers)
+    }
+
+    func sortAndPrintArray(array: [Any]) {
+        if let array = array as? [Int] {
+            let sortedArray = array.sorted(by: >)
+            print(sortedArray)
+        }
     }
 }
