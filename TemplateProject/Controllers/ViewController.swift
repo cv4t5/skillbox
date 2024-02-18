fb23972a24b701b66216bf3d77ebdfc397e81eb5
Roman Zakharchenko <r.zaharchenko23@gmail.com>
2024-02-19 00:18:17 +0200

    init
    initial commit for upload project

---
+++ TemplateProject/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TemplateProject
+//
+//  Created by Роман Захарченко on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
